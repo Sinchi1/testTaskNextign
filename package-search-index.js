74d8766e20c22ffb013fd9d6eed7f8f33dbf7ed9
Sinchi <goshat17@gmail.com>
2025-03-24 01:18:31 +0300

    Забыл про jar + забыл удалить файлы javadoc из корня

--- package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.Truskovski.testTask"},{"l":"com.Truskovski.testTask.Controlers"},{"l":"com.Truskovski.testTask.DataBase"},{"l":"com.Truskovski.testTask.Fabrics"},{"l":"com.Truskovski.testTask.Objects"},{"l":"com.Truskovski.testTask.Objects.DTO"},{"l":"com.Truskovski.testTask.Service"}];updateSearchResults();
